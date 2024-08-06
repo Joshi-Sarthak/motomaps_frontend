f034d782e260a0f717bb1f068cae9e303c3218c2
AtharvaBhore <atharvabhore6@gmail.com>
2024-08-06 21:53:17 +0530

    Fix search bar

--- src/components/ui/Search.jsx
+++ src/components/ui/Search.jsx
@@ -169,7 +169,7 @@ export function PlaceholdersAndVanishInput({ placeholders, onChange, onSubmit })
 		<form
 			className={cn(
 				"w-3/4 lg:w-full relative max-w-xl mx-auto bg-zinc-800 h-12 rounded-full overflow-hidden shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),_0px_1px_0px_0px_rgba(25,28,33,0.02),_0px_0px_0px_1px_rgba(25,28,33,0.08)] transition duration-200",
-				value && "bg-gray-50"
+				value && "bg-zinc-800"
 			)}
 			onSubmit={handleSubmit}
 		>
